1049fc7204274b9eb49005df48cd6375fc96ee42
Swissdkb <Swissdkb@hotmail.com>
2019-01-05 21:16:53 +0800

    WinFormHelloWorld

--- WinFormsHelloWorld/WinFormsHelloWorld/Form1.Designer.cs
+++ WinFormsHelloWorld/WinFormsHelloWorld/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonSayHello = new System.Windows.Forms.Button();
+            this.textBoxShowHello = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSayHello
+            // 
+            this.buttonSayHello.AccessibleName = "";
+            this.buttonSayHello.Location = new System.Drawing.Point(81, 180);
+            this.buttonSayHello.Name = "buttonSayHello";
+            this.buttonSayHello.Size = new System.Drawing.Size(396, 23);
+            this.buttonSayHello.TabIndex = 0;
+            this.buttonSayHello.Text = "Click Me";
+            this.buttonSayHello.UseVisualStyleBackColor = true;
+            this.buttonSayHello.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxShowHello
+            // 
+            this.textBoxShowHello.Location = new System.Drawing.Point(81, 153);
+            this.textBoxShowHello.Name = "textBoxShowHello";
+            this.textBoxShowHello.Size = new System.Drawing.Size(396, 21);
+            this.textBoxShowHello.TabIndex = 1;
+            this.textBoxShowHello.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(582, 450);
+            this.Controls.Add(this.textBoxShowHello);
+            this.Controls.Add(this.buttonSayHello);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonSayHello;
+        private System.Windows.Forms.MaskedTextBox textBoxShowHello;
     }
 }
 
--- WinFormsHelloWorld/WinFormsHelloWorld/Form1.cs
+++ WinFormsHelloWorld/WinFormsHelloWorld/Form1.cs
@@ -16,5 +16,9 @@ namespace WinFormsHelloWorld
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxShowHello.Text = "Hello,World";
+        }
     }
 }
